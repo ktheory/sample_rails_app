c0bcf4de0bffd647427faa1f317704d3ad2da517
Aaron Suggs <aaron@ktheory.com>
2012-03-04 19:43:37 -0500

    ruby 1.8 syntax

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-SampleRailsApp::Application.config.session_store :cookie_store, key: '_sample_rails_app_session'
+SampleRailsApp::Application.config.session_store :cookie_store, :key => '_sample_rails_app_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
